112d8095cd290f57f93c9d3f05a43b9d6a39d80b
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-12-12 16:46:25 -0300

    First Commit

---
+++ holaMundo.php
@@ -0,0 +1,27 @@
+<?php
+
+// Comentario PHP
+// Alert, console.log, etiquetas HTML
+echo 'Hola Mundo PHP';
+
+
+echo '<form>
+
+        <label>
+            <input placeholder="Name" type="text" id="name" name="user_name">
+        </label>
+
+        <label>
+            <input placeholder="email" type="text" id="email" name="user_email">
+        </label>
+
+        <label>
+            <input placeholder="password" type="text" id="password" name="user_password">
+        </label>
+
+        <label>
+            <textarea cols="20" rows="20" type="text" id="msg" name="msg"></textarea>
+        </label>
+
+      </form>
+';
